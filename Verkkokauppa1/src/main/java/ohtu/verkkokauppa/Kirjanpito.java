b42ee697ba42f860f2c5cac1fc886bdf35d45b5f
ankatus <antti.j.kataja@gmail.com>
2017-11-11 23:01:13 +0200

    done

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -1,10 +1,13 @@
 
 package ohtu.verkkokauppa;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 
+@Component
 public class Kirjanpito implements KirjanpitoInterface {
-    
+
     private ArrayList<String> tapahtumat;
 
     public Kirjanpito() {
